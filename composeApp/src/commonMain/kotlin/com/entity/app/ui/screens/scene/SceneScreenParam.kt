ae10c6b6b9b70710c843f34d8221d35b7ea7c923
adtarassov <adtarassov@yandex-team.ru>
2023-07-08 15:22:27 +0300

    crash fix with JavaSerializable

--- composeApp/src/commonMain/kotlin/com/entity/app/ui/screens/scene/SceneScreenParam.kt
+++ composeApp/src/commonMain/kotlin/com/entity/app/ui/screens/scene/SceneScreenParam.kt
@@ -1,9 +1,10 @@
 package com.entity.app.ui.screens.scene
 
+import cafe.adriel.voyager.core.lifecycle.JavaSerializable
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class SceneScreenParam(
   val sceneId: String,
   val isPromo: Boolean
-)
+) : JavaSerializable
